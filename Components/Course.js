e8b4225c2149a7cdc996d97bf2396057b40e0e3c
Rahul Singh Gurjar <78016968+0901IT201046@users.noreply.github.com>
2022-10-26 02:06:05 +0530

    Made Changes with Progress Monitor and also inserted SignIn Page.

--- Components/Course.js
+++ Components/Course.js
@@ -1,13 +1,13 @@
-import { StyleSheet, Text, View, ImageBackground, Image, TouchableOpacity, } from 'react-native'
-import React from 'react'
-import { Modalize } from 'react-native-modalize'
-import { ScrollView, TextInput } from 'react-native-gesture-handler'
-import CourseList from './CourseList'
-import { onPress } from 'deprecated-react-native-prop-types/DeprecatedTextPropTypes'
+import { StyleSheet, Text, View, ImageBackground, Image, TouchableOpacity, } from 'react-native';
+import React from 'react';
+import { Modalize } from 'react-native-modalize';
+import { ScrollView, TextInput } from 'react-native-gesture-handler';
+import CourseList from './CourseList';
+import { onPress } from 'deprecated-react-native-prop-types/DeprecatedTextPropTypes';
 
 const Course = ({ navigation }) => {
-  
-  
+
+
   return (
     <ImageBackground source={require('../assets/images/cat.png')}
       style={{ width: "100%", height: "100%" }}>
@@ -23,11 +23,11 @@ const Course = ({ navigation }) => {
 
 
       }}>
-        <TouchableOpacity onPress={()=>{navigation.navigate("Home")}}>
+        <TouchableOpacity onPress={() => { navigation.navigate("Home") }}>
           <Image
             source={require('../assets/images/a1.png')}
             style={{ width: 23, height: 20 }}
-            
+
           />
         </TouchableOpacity>
         <TouchableOpacity style={{ paddingHorizontal: 323 }}>
@@ -49,71 +49,71 @@ const Course = ({ navigation }) => {
       }}>
         UI/UX Cources
       </Text>
-     
-      
-        <ScrollView style={{
-          marginTop:30,
-          backgroundColor:"white",
-          width:"100%",
-          borderTopLeftRadius:20,
-          borderTopRightRadius:20,
-          
 
-        }}
-        >
-        
-          {/* <Text onPress={()=>{console.log("pressed"),navigation.navigate("Home")}}>hello world
+
+      <ScrollView style={{
+        marginTop: 30,
+        backgroundColor: "white",
+        width: "100%",
+        borderTopLeftRadius: 20,
+        borderTopRightRadius: 20,
+
+
+      }}
+      >
+
+        {/* <Text onPress={()=>{console.log("pressed"),navigation.navigate("Home")}}>hello world
 
           </Text> */}
-        
-          <CourseList
-            onPress={()=>{console.log("pressed"),navigation.navigate('Xd')}}
-            img={require('../assets/images/xd.png')}
-            title="Adobe XD Prototyping"
-            bg="#fdddf3"
-            
-          />
-          
-          <CourseList
-          onPress={()=>{console.log("pressed"),navigation.navigate('Xd')}}
-            img={require('../assets/images/sketch.png')}
-            title="Sketch shortcuts and tricks"
-            bg="#fef8e3"
-          />
-          <CourseList
-          onPress={()=>{console.log("pressed"),navigation.navigate('Xd')}}
-            img={require('../assets/images/ae.png')}
-            title="UI Motion Design in After Effects"
-            bg="#fcf2ff"
-          />
-          <CourseList
-          onPress={()=>{console.log("pressed"),navigation.navigate('Xd')}}
-            img={require('../assets/images/f.png')}
-            title="Figma Essentials"
-            bg="#fff0ee"
-          />
-          <CourseList
-          onPress={()=>{console.log("pressed"),navigation.navigate('Xd')}}
-            img={require('../assets/images/ps.png')}
-            title="Adobe Photoshop. Retouching"
-            bg="#fdddf3"
-          />
-          <CourseList
-          onPress={()=>{console.log("pressed"),navigation.navigate('Xd')}}
-            img={require('../assets/images/sketch.png')}
-            title="Sketch shortcuts and tricks"
-            bg="#fef8e3"
-          />
-          <CourseList
-          onPress={()=>{console.log("pressed"),navigation.navigate('Xd')}}
-            img={require('../assets/images/ae.png')}
-            title="UI Motion Design in After Effects"
-            bg="#fcf2ff"
-          />
-          </ScrollView>
-        
-      
-      
+
+        <CourseList
+          onPress={() => { console.log("pressed"), navigation.navigate('Xd') }}
+          img={require('../assets/images/xd.png')}
+          title="Adobe XD Prototyping"
+          bg="#fdddf3"
+
+        />
+
+        <CourseList
+          onPress={() => { console.log("pressed"), navigation.navigate('Xd') }}
+          img={require('../assets/images/sketch.png')}
+          title="Sketch shortcuts and tricks"
+          bg="#fef8e3"
+        />
+        <CourseList
+          onPress={() => { console.log("pressed"), navigation.navigate('Xd') }}
+          img={require('../assets/images/ae.png')}
+          title="UI Motion Design in After Effects"
+          bg="#fcf2ff"
+        />
+        <CourseList
+          onPress={() => { console.log("pressed"), navigation.navigate('Xd') }}
+          img={require('../assets/images/f.png')}
+          title="Figma Essentials"
+          bg="#fff0ee"
+        />
+        <CourseList
+          onPress={() => { console.log("pressed"), navigation.navigate('Xd') }}
+          img={require('../assets/images/ps.png')}
+          title="Adobe Photoshop. Retouching"
+          bg="#fdddf3"
+        />
+        <CourseList
+          onPress={() => { console.log("pressed"), navigation.navigate('Xd') }}
+          img={require('../assets/images/sketch.png')}
+          title="Sketch shortcuts and tricks"
+          bg="#fef8e3"
+        />
+        <CourseList
+          onPress={() => { console.log("pressed"), navigation.navigate('Xd') }}
+          img={require('../assets/images/ae.png')}
+          title="UI Motion Design in After Effects"
+          bg="#fcf2ff"
+        />
+      </ScrollView>
+
+
+
 
 
 
--- Components/Navigator.js
+++ Components/Navigator.js
@@ -1,9 +1,10 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import Home from './Home'
-import Course from './Course'
-import VideoPlayer from './VideoPlayer'
-import Xd from './Xd'
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import Home from './Home';
+import Course from './Course';
+import VideoPlayer from './VideoPlayer';
+import Xd from './Xd';
+import SignInScreen from '../src/screens/SignInScreen/SignInScreen';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
@@ -14,13 +15,14 @@ const Stack = createNativeStackNavigator();
 const Navigator = () => {
   return (<NavigationContainer>
     <Stack.Navigator>
-      <Stack.Screen name="Home" component={Home} options={{headerShown: false}} />
-      <Stack.Screen name="Course" component={Course} options={{headerShown: false}} />
-      <Stack.Screen name="VideoPlayer" component={VideoPlayer} options={{headerShown: false}}/>
-      <Stack.Screen name="Xd" component={Xd} options={{headerShown: false}} />
+      <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
+      <Stack.Screen name="Course" component={Course} options={{ headerShown: false }} />
+      <Stack.Screen name="VideoPlayer" component={VideoPlayer} options={{ headerShown: false }} />
+      <Stack.Screen name="Xd" component={Xd} options={{ headerShown: false }} />
+      <Stack.Screen name="SignIn" component={SignInScreen} options={{ headerShown: false }} />
     </Stack.Navigator>
   </NavigationContainer>
   )
 }
 
-export default Navigator
+export default Navigator;
