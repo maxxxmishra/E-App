0cda726e340a20ea26a055cdf7141331ee7b00b8
Rahul Singh Gurjar <78016968+0901IT201046@users.noreply.github.com>
2022-10-27 01:29:50 +0530

    Create NewPasswordScreen.js

---
+++ src/screens/NewPasswordScreen/NewPasswordScreen.js
@@ -0,0 +1,100 @@
+import { View, Text, StyleSheet, ScrollView} from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import React, {useState} from 'react';
+import Logo from '../../../assets/images/Dummy_Logo.png';
+import CustomInput from '../../../Components/CustomInput';
+import CustomButton from '../../../Components/CustomButton';
+import SocialSignInButtons from '../../../Components/SocialSignInButtons';
+import {useNavigation} from '@react-navigation/native';
+import {useForm} from 'react-hook-form';
+
+const NewPasswordScreen= () => {
+    const {control, handleSubmit} = useForm();
+
+    const navigation = useNavigation();
+     
+    const onSubmitPressed = () =>{
+        navigation.navigate("Home");
+    };
+
+    const onSignInPressed =() =>{
+        navigation.navigate("SignIn");
+    };
+
+  return (
+    <ScrollView 
+        showsVerticalScrollIndicator={false} 
+        style={styles.Scroll}
+     >
+    <SafeAreaView style={styles.container}>
+    <View style={StyleSheet.root}>
+      <Text style={styles.title}>Reset your password</Text>
+      <View style={styles.dist}>
+
+      <CustomInput 
+      name="code"
+      control={control}
+      placeholder="Code" 
+      rules={{required: 'Code is required'}}
+       />
+     <CustomInput 
+      name = "password"
+      control={control}
+      placeholder="Enter your new password" 
+      secureTextEntry={true}
+      rules={{
+        required:'Password is required',
+        minLength:{
+          value: 8,
+          message: 'Password should be minimum 8 characters long',
+        },
+        }}
+       />
+      <CustomButton text="Submit" 
+        onPress={handleSubmit(onSubmitPressed)}/>
+
+        <CustomButton 
+        text="Back to Sign in" 
+        onPress={onSignInPressed}
+        type="TERTIARY"
+        />
+      </View>
+    </View>
+    </SafeAreaView>
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+    container:{
+        backgroundColor: '#ffffff',
+        flex: 1,
+    },
+    root:{
+        alignItems: 'center',
+        padding: 20,
+
+    },
+    dist:{
+        marginTop: 30,
+    },
+    title:{
+        fontSize: 30,
+        fontWeight: 'bold',
+        color: '#051c60',
+        margin: 10,
+    },
+    Scroll:{
+        backgroundColor:'#fff',
+        flex: 1,
+    },
+    text: {
+        color: 'gray',
+        margin: 10,
+    },
+    link:{
+        color: '#fdb075',
+    },
+});
+
+export default NewPasswordScreen;
