a4e51eb4b32be880b6088bb8e0304efc9ff031b5
Rahul Singh Gurjar <78016968+0901IT201046@users.noreply.github.com>
2022-10-27 01:38:31 +0530

    Update Navigator.js

--- Components/Navigator.js
+++ Components/Navigator.js
@@ -5,9 +5,12 @@ import Course from './Course';
 import VideoPlayer from './VideoPlayer';
 import Xd from './Xd';
 import SignInScreen from '../src/screens/SignInScreen/SignInScreen';
+import SignUpScreen from '../src/screens/SignUpScreen/SignUpScreen';
+import ConfirmEmailScreen from '../src/screens/ConfirmEmailScreen/ConfirmEmailScreen';
+import ForgotPasswordScreen from '../src/screens/ForgotPasswordScreen/ForgotPasswordScreen';
+import NewPasswordScreen from '../src/screens/NewPasswordScreen/NewPasswordScreen';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import SignInScreen from '../src/screens/SignInScreen/SignInScreen';
 
 
 const Stack = createNativeStackNavigator();
@@ -16,19 +19,18 @@ const Stack = createNativeStackNavigator();
 const Navigator = () => {
   return (<NavigationContainer>
     <Stack.Navigator>
-<<<<<<< HEAD
       <Stack.Screen name="Home" component={Home} options={{headerShown: false}} />
       <Stack.Screen name="Course" component={Course} options={{headerShown: false}} />
       <Stack.Screen name="VideoPlayer" component={VideoPlayer} options={{headerShown: false}}/>
       <Stack.Screen name="Xd" component={Xd} options={{headerShown: false}} />
-=======
-      <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
-      <Stack.Screen name="Course" component={Course} options={{ headerShown: false }} />
-      <Stack.Screen name="VideoPlayer" component={VideoPlayer} options={{ headerShown: false }} />
-      <Stack.Screen name="Xd" component={Xd} options={{ headerShown: false }} />
->>>>>>> f59c3d7b66e67ac8b045a0b48077f26def8ef1de
-      <Stack.Screen name="SignIn" component={SignInScreen} options={{ headerShown: false }} />
+      <Stack.Screen name="SignIn" component={SignInScreen} options={{headerShown: false}} />
+      <Stack.Screen name="SignUp" component={SignUpScreen} options={{headerShown: false}} />
+      <Stack.Screen name="ConfirmEmail" component={ConfirmEmailScreen} options={{headerShown: false}} />
+      <Stack.Screen name="ForgotPassword" 
+      component={ForgotPasswordScreen} options={{headerShown: false}} />
+      <Stack.Screen name="NewPassword" component={NewPasswordScreen} options={{headerShown: false}} />
     </Stack.Navigator>
+
   </NavigationContainer>
   )
 }
