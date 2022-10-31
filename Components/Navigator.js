f06b92f48629ba84321525d6cd1e98ca99feae28
Rahul Singh Gurjar <78016968+0901IT201046@users.noreply.github.com>
2022-10-31 11:52:36 +0530

    Update Navigator.js

--- Components/Navigator.js
+++ Components/Navigator.js
@@ -1,5 +1,5 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import { View, ActivityIndicator } from 'react-native';
+import React, {useState, useEffect} from 'react';
 import Home from './Home';
 import Course from './Course';
 import VideoPlayer from './VideoPlayer';
@@ -11,28 +11,69 @@ import ForgotPasswordScreen from '../src/screens/ForgotPasswordScreen/ForgotPass
 import NewPasswordScreen from '../src/screens/NewPasswordScreen/NewPasswordScreen';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {Auth, Hub} from 'aws-amplify';
 
 
 const Stack = createNativeStackNavigator();
 
 
 const Navigator = () => {
-  return (<NavigationContainer>
-    <Stack.Navigator>
+  const [user, setUser] = useState(undefined);
+
+  const checkUser = async () => {
+    try {
+      const authUser = await Auth.currentAuthenticatedUser({bypassCache: true});
+      setUser(authUser);
+    } catch (e) {
+      setUser(null);
+    }
+  };
+
+  useEffect(() => {
+    checkUser();
+  }, []);
+
+  useEffect(() => {
+    const listener = data => {
+      if (data.payload.event === 'signIn' || data.payload.event === 'signOut') {
+        checkUser();
+      }
+    };
+
+    Hub.listen('auth', listener);
+    return () => Hub.remove('auth', listener);
+  }, []);
+
+  if (user === undefined) {
+    return (
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <ActivityIndicator size="large"/>
+      </View>
+    );
+  }
+  return (
+  <NavigationContainer>
+    <Stack.Navigator screenOptions={{headerShown: false}} >
+      {user ? (
+      <>
       <Stack.Screen name="Home" component={Home} options={{headerShown: false}} />
       <Stack.Screen name="Course" component={Course} options={{headerShown: false}} />
       <Stack.Screen name="VideoPlayer" component={VideoPlayer} options={{headerShown: false}}/>
       <Stack.Screen name="Xd" component={Xd} options={{headerShown: false}} />
+      </>
+      ) : (
+      <>
       <Stack.Screen name="SignIn" component={SignInScreen} options={{headerShown: false}} />
       <Stack.Screen name="SignUp" component={SignUpScreen} options={{headerShown: false}} />
       <Stack.Screen name="ConfirmEmail" component={ConfirmEmailScreen} options={{headerShown: false}} />
       <Stack.Screen name="ForgotPassword" 
       component={ForgotPasswordScreen} options={{headerShown: false}} />
       <Stack.Screen name="NewPassword" component={NewPasswordScreen} options={{headerShown: false}} />
+      </>
+      )}
     </Stack.Navigator>
-
   </NavigationContainer>
-  )
-}
+  );
+};
 
 export default Navigator;
