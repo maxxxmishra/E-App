8bdc8df8d5660b47c5137d4742f94b11dbc2baef
Rahul Singh Gurjar <78016968+0901IT201046@users.noreply.github.com>
2022-11-15 15:47:05 +0530

    Update C.js

--- Api/C.js
+++ Api/C.js
@@ -1,54 +1,152 @@
 const C = [
-    {
+    {   
         ID:"1",
-        "title":"UI Motion Design in After Effects",
+        "title":"Theory of Computation",
         "bg":"#fcf2ff",
         img:require('../assets/images/xd.png'),
         subtitle : [
-            {title : "Introduction" ,time :"3 minutes, 10 seconds" ,videoId :"EqsFdDmYzxU"},
-            {title : "What is UI" ,time :"4 minutes, 10 seconds",videoId :"Hiu4YulfJ08"},
-            {title : "Types of UI" ,time :"5 minutes, 10 seconds",videoId :"EqsFdDmYzxU"},
-            {title : "How to Read IT" ,time :"6 minutes, 10 seconds",videoId :"EqsFdDmYzxU"},
-            {title : "Why Should Read IT" ,time :"7 minutes, 10 seconds",videoId :"EqsFdDmYzxU"},
-            {title : "Must Read IT" ,time :"3 minutes, 15 seconds",videoId :"EqsFdDmYzxU"},
-            {title : "At the end" ,time :"3 minutes, 14 seconds",videoId :"EqsFdDmYzxU"},
-            {title : "Summary" ,time :"3 minutes, 13 seconds",videoId :"EqsFdDmYzxU"},
-        ]
+            {title : "INTRODUCTION" ,time :"5 minutes, 14 seconds" ,videoId :"CSsDOIAVwnA"},
+            {title : "UNIT-1" ,time :"1 Hour , 54 minutes",videoId :"tLdDQa_CvcU"},
+            {title : "UNIT-2" ,time :"1 Hour , 48 minutes",videoId :"PaQbkrT1_0w"},
+            {title : "UNIT-3" ,time :"1 Hour , 45 minutes",videoId :"1KJI_ibRKOA"},
+            {title : "UNIT-4" ,time :"1 Hour , 21 minutes",videoId :"T7eNEZe_clQ"},
+            {title : "UNIT-5" ,time :"1 Hour , 37 minutes",videoId :"9Lnxn_HAo8M"},
+            {title : "SUMMARY" ,time :"7 minutes, 31 seconds",videoId :"BBuBv3P2aSs"},
+            
+        ],
+        des : "The Theory of Constraints is a methodology for identifying the most important limiting factor (i.e., constraint) that stands in the way of achieving a goal and then systematically improving that constraint until it is no longer the limiting factor. In manufacturing, the constraint is often referred to as a bottleneck.The Theory of Constraints takes a scientific approach to improvement. It hypothesizes that every complex system, including manufacturing processes, consists of multiple linked activities, one of which acts as a constraint upon the entire system (i.e., the constraint activity is the “weakest link in the chain”)."
     },
-    {
+    {   
         ID:"2",
+        "title":"Business Environment ",
+        "bg":"#fcf2ff",
         img:require('../assets/images/sketch.png'),
-          title:"Sketch shortcuts and tricks",
-          bg:"#fef8e3",
-          subtitle : [
-            {title : "Introduction" ,time :"3 minutes, 10 seconds" ,videoId :"EqsFdDmYzxU"},
-            {title : "What is UI" ,time :"4 minutes, 10 seconds",videoId :"Hiu4YulfJ08"},
-            {title : "Types of UI" ,time :"5 minutes, 10 seconds",videoId :"EqsFdDmYzxU"},
-            {title : "How to Read IT" ,time :"6 minutes, 10 seconds",videoId :"EqsFdDmYzxU"},
-            {title : "Why Should Read IT" ,time :"7 minutes, 10 seconds",videoId :"EqsFdDmYzxU"},
-            {title : "Must Read IT" ,time :"3 minutes, 15 seconds",videoId :"EqsFdDmYzxU"},
-            {title : "At the end" ,time :"3 minutes, 14 seconds",videoId :"EqsFdDmYzxU"},
-            {title : "Summary" ,time :"3 minutes, 13 seconds",videoId :"EqsFdDmYzxU"},
-        ]
+        subtitle : [
+            {title : "INTRODUCTION" ,time :"5 minutes, 14 seconds" ,videoId :"CSsDOIAVwnA"},
+            {title : "UNIT-1" ,time :"1 Hour , 10 minutes",videoId :"BokltOUVJRs"},
+            {title : "UNIT-2" ,time :"1 Hour , 32 minutes",videoId :"QTz5QfueeD8&t=1s"},
+            {title : "UNIT-3" ,time :"1 Hour , 45 minutes",videoId :"zKc1AQP3KmA"},
+            {title : "UNIT-4" ,time :"1 Hour , 37 minutes",videoId :"kxzHm4XAETI"},
+            {title : "UNIT-5" ,time :"1 Hour , 14 minutes",videoId :"MgCueEGP6Mo"},
+            {title : "SUMMARY" ,time :"28 minutes, 42 seconds",videoId :"mkjK_BDrPDM"},
+            
+        ],
+        des : "Business environment refers to “the total of all things external to firms and industries which affect their organisation and operation. Bayord O. Wheeler. Business environment encompasses the climate or set of conditions, economic, social, political, or institutional in which business operations are conducted.Business environment helps in identifying business opportunities, tapping useful resources, assists in planning, and improves the overall performance, growth, and profitability of the business. There are various types of Business Environment like Micro Environment and Macro Environment."
     },
     {
         ID:"3",
-        img:require('../assets/images/ae.png'),
-          title:"UI Motion Design in After Effects",
-          bg:"#fcf2ff",
+        img:require('../assets/images/sketch.png'),
+          title:"Discrete Mathematics",
+          bg:"#fef8e3",
           subtitle : [
-            {title : "Introduction" ,time :"3 minutes, 10 seconds" ,videoId :"EqsFdDmYzxU"},
-            {title : "What is UI" ,time :"4 minutes, 10 seconds",videoId :"Hiu4YulfJ08"},
-            {title : "Types of UI" ,time :"5 minutes, 10 seconds",videoId :"EqsFdDmYzxU"},
-            {title : "How to Read IT" ,time :"6 minutes, 10 seconds",videoId :"EqsFdDmYzxU"},
-            {title : "Why Should Read IT" ,time :"7 minutes, 10 seconds",videoId :"EqsFdDmYzxU"},
-            {title : "Must Read IT" ,time :"3 minutes, 15 seconds",videoId :"EqsFdDmYzxU"},
-            {title : "At the end" ,time :"3 minutes, 14 seconds",videoId :"EqsFdDmYzxU"},
-            {title : "Summary" ,time :"3 minutes, 13 seconds",videoId :"EqsFdDmYzxU"},
-        ]
+            {title : "INTRODUCTION" ,time :"12 minutes, 31 seconds" ,videoId :"P2W4oeOEvpQ"},
+            {title : "UNIT-1" ,time :"42 minutes, 52 seconds",videoId :"TMhmUDMQTXI"},
+            {title : "UNIT-2" ,time :"1 hours, 9 minutes",videoId :"DmkGEc0tKbY"},
+            {title : "UNIT-3" ,time :"28 minutes, 42 seconds",videoId :"zKc1AQP3KmA"},
+            {title : "UNIT-4" ,time :"32 minutes, 57 seconds",videoId :"9gmApuESyF8"},
+            {title : "UNIT-5" ,time :"21 minutes, 34 seconds",videoId :"0zOaMRyeO2I"},
+            {title : "SUMMARY" ,time :"24 minutes, 55 seconds",videoId :"wGLTV8MgLlA"},
+            
+        ],
+        des : "This course enables students to strengthen and increase the understanding of discrete mathematics with special emphasis on computer science applications. Topics include sets, number systems, the nature of proof, formal logic, functions and relations, combinatorics, recurrence relations, trees and Boolean algebra."
     },
     {
         ID:"4",
+        img:require('../assets/images/sketch.png'),
+          title:"Python",
+          bg:"#fef8e3",
+          subtitle : [
+            {title : "INTRODUCTION" ,time :"26 minutes, 36 seconds" ,videoId :"_JovpWD9LN8"},
+            {title : "PYTHON SCRIPTS" ,time :"28 minutes, 06 seconds",videoId :"gf_Ffhmt-Uo"},
+            {title : "PYTHON VARIABLES" ,time :"27 minutes, 45 seconds",videoId :"Q8bzd9x-PWM"},
+            {title : "FUNCTION" ,time :"27 minutes, 01 seconds",videoId :"Q0HpzOv0vJc"},
+            {title : "EXPRESSION" ,time :"26 minutes, 10 seconds",videoId :"GZmuwwYx9w8"},
+            {title : "LOOP" ,time :"26 minutes, 30 seconds",videoId :"QQPW55P9BwE"},
+            {title : "CONDITIONAL" ,time :"21 minutes, 46 seconds",videoId :"hhTTV4Z-_-s"},
+            {title : "OVERVIEW" ,time :"16 minutes, 09 seconds",videoId :"gGSnbhAn43I"},
+            
+        ],
+        des : "Python is a computer programming language often used to build websites and software, automate tasks, and conduct data analysis. Python is a general-purpose language, meaning it can be used to create a variety of different programs and isn't specialized for any specific problems.Python is an interpreted, object-oriented, high-level programming language with dynamic semantics developed by Guido van Rossum. It was originally released in 1991. Designed to be easy as well as fun, the name Python is a nod to the British comedy group Monty Python."
+    },
+    {
+        ID:"5",
+        img:require('../assets/images/ae.png'),
+          title:"Engineering Mathematics-1",
+          bg:"#fcf2ff",
+          subtitle : [
+            {title : "Lec-1" ,time :"38 minutes, 50 seconds",videoId :"RtuNwdHDEAw"},
+            {title : "Lec-2" ,time :"1hours , 06 minutes",videoId :"BGWcsrhxaTU"},
+            {title : "Lec-3" ,time :"34 minutes, 40 seconds",videoId :"GWtkVPuSVF8"},
+            {title : "Lec-4" ,time :"33 minutes, 29 seconds",videoId :"3nafrDiN0gU"},
+            {title : "Lec-5" ,time :"32 minutes, 10 seconds",videoId :"EEpj1zC8HkE"},
+            {title : "Lec-6" ,time :"41 minutes, 21 seconds",videoId :"e9PIlsMLz4M"},
+            {title : "Lec-7" ,time :"35 minutes, 24 seconds",videoId :"9bb7JV1owrQ"},
+            {title : "Lec-8" ,time :"35 minutes, 36 seconds",videoId :"NqMoo81TJaw"},
+            {title : "Lec-9" ,time :"32 minutes, 05 seconds",videoId :"_u0y7NgSOHI"},
+            {title : "Lec-10" ,time :"36 minutes, 06 seconds",videoId :"D5V0NXtWGtQ"},
+            
+            
+            
+
+        ],
+        des : "The course consists of topics in differential calculus,integral calculus, linear algebra and differential equations with applications to various engineering problems. This course will cover the following main topics: Mean Value Theorems; Indeterminate Forms; Taylor's and Maclaurin's Theorems.Engineering mathematics is the art of applying maths to complex real-world problems; combining mathematical theory, practical engineering and scientific computing to address today's technological challenges."
+    },
+    {
+        ID:"6",
+        img:require('../assets/images/ae.png'),
+          title:"Engineering Mathematics-2",
+          bg:"#fcf2ff",
+          subtitle : [
+            {title : "Syllabus" ,time :"8 minutes, 47 seconds" ,videoId :"hBoEBuTPwYw"},
+            {title : "Lec-1" ,time :"23 minutes, 26 seconds",videoId :""},
+            {title : "Lec-2" ,time :"37 minutes, 19 seconds",videoId :"xutudIaLVwg"},
+            {title : "Lec-3" ,time :"30 minutes, 02 seconds",videoId :"jANb7IhK17Y"},
+            {title : "Lec-4" ,time :"25 minutes, 46 seconds",videoId :"volwcI5hKfA"},
+            {title : "Lec-5" ,time :"19 minutes, 30 seconds",videoId :"ZwYgDhCB2tk"},
+            {title : "Lec-6" ,time :"41 minutes, 54 seconds",videoId :"Ml8GfRItlOg"},
+            {title : "Lec-7" ,time :"27 minutes, 05 seconds",videoId :"oNTr_51CKfY"},
+        ],
+        des : "This course is about the basic mathematics that is fundamental and essential component in all streams of undergraduate studies in sciences and engineering. The course consists of topics in complex analysis,numerical analysis, vector calculus and transform techniques with applications to various engineering problems.Engineering mathematics is the art of applying maths to complex real-world problems; combining mathematical theory, practical engineering and scientific computing to address today's technological challenges."
+    },
+    {
+        ID:"7",
+        img:require('../assets/images/ae.png'),
+          title:"Engineering Mathematics-3",
+          bg:"#fcf2ff",
+          subtitle : [
+            {title : "Lec-1" ,time :"1 hours , 06 minutes",videoId :"O_7zXr1sR00"},
+            {title : "Lec-2" ,time :"1 hours , 09 minutes",videoId :"F50eZWHuHUo"},
+            {title : "Lec-3" ,time :"1 hours , 04 minutes",videoId :"q0rsuQSxj1U"},
+            {title : "Lec-4" ,time :"1 hours , 00 minutes",videoId :"kHKogHryzKc"},
+            {title : "Lec-5" ,time :"59 minutes, 57 seconds",videoId :"Ga4Tb8BLRws"},
+            {title : "Lec-6" ,time :"1 hours , 15 minutes",videoId :"VWtx8b7WZ30"},
+            {title : "Lec-7" ,time :"56 minutes, 55 seconds",videoId :"kY_lqc8YmqI"},
+            {title : "Lec-8" ,time :"47 minutes, 41 seconds",videoId :"8W791-Xghrk"},
+
+        ],
+        des : "Image result for Engineering Mathematics-3 description in briefEngineering mathematics is the art of applying maths to complex real-world problems; combining mathematical theory, practical engineering and scientific computing to address today's technological challenges.Definition Fourier integral theorem(without proof) - Fourier sine and cosine integrals Fourier transforms –Properties of Fourier transforms Convolution theorem Parseval's identity for Fourier transforms Relation between Fourier and Laplace transforms Fourier transforms of the derivatives of ..."
+    },
+    {
+        ID:"8",
+        img:require('../assets/images/ae.png'),
+          title:"OOPS",
+          bg:"#fcf2ff",
+          subtitle : [
+            {title : "OOP-1" ,time :"1 hours , 42 minutes",videoId :"BSVKUk58K6U"},
+            {title : "OOP-2" ,time :"1 hours , 19 minutes",videoId :"_Ya6CN13t8k"},
+            {title : "OOP-3" ,time :"1 hours , 12 minutes",videoId :"46T2wD3IuhM"},
+            {title : "OOP-4" ,time :"50 minutes , 45 seconds",videoId :"W145DXs8fFg"},
+            {title : "OOP-5" ,time :"1 hours , 10 minutes",videoId :"rgHZa7-Dibg"},
+            {title : "OOP-6" ,time :"1 hours , 31 minutes",videoId :"OY2lPr8h93U"},
+            {title : "OOP-7" ,time :"26 minutes, 40 seconds",videoId :"9ogGan-R1pc"},
+            
+
+        ],
+        des : "Object-oriented programming (OOP) is a computer programming model that organizes software design around data, or objects, rather than functions and logic. An object can be defined as a data field that has unique attributes and behavior.System (OOPs) is a programming concept that works on the principles of abstraction, encapsulation, inheritance, and polymorphism. It allows users to create objects they want and create methods to handle those objects."
+    },
+
+    {
+        ID:"9",
         img:require('../assets/images/f.png'),
         title:"Figma Essentials",
         bg:"#fff0ee",
@@ -63,7 +161,7 @@ const C = [
             {title : "Summary" ,time :"3 minutes, 13 seconds",videoId :"EqsFdDmYzxU"},
         ]
     },
-    {   ID:"5",
+    {   ID:"10",
         img:require('../assets/images/ps.png'),
           title:"Adobe Photoshop. Retouching",
           bg:"#fdddf3",
@@ -78,7 +176,7 @@ const C = [
             {title : "Summary" ,time :"3 minutes, 13 seconds",videoId :"EqsFdDmYzxU"},
         ]
     },
-    {   ID:"6",
+    {   ID:"11",
         img:require('../assets/images/sketch.png'),
         title:"Sketch shortcuts and tricks",
         bg:"#fef8e3",
@@ -93,7 +191,7 @@ const C = [
             {title : "Summary" ,time :"3 minutes, 13 seconds",videoId :"EqsFdDmYzxU"},
         ]
     },
-    {   ID:"7",
+    {   ID:"12",
         img:require('../assets/images/ae.png'),
           title:"UI Motion Design in After Effects",
           bg:"#fcf2ff",
@@ -107,7 +205,8 @@ const C = [
             {title : "At the end" ,time :"3 minutes, 14 seconds",videoId :"EqsFdDmYzxU"},
             {title : "Summary" ,time :"3 minutes, 13 seconds",videoId :"EqsFdDmYzxU"},
         ]
-    }
+    },
+    
     
 ]
 
